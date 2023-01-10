22945ed83420ef3d3049f436adb79ca8da150793
Ben Oram <b@oram.co>
2023-01-10 20:42:05 +0000

    Added watchtower docker container

--- docker.tf
+++ docker.tf
@@ -1,28 +1,25 @@
-# resource "docker_image" "cloudflared" {
-#   name = "cloudflare/cloudflared:latest"
-# }
+data "docker_registry_image" "watchtower" {
+  name = "containrrr/watchtower:latest"
+}
 
+resource "docker_image" "watchtower" {
+  name = data.docker_registry_image.watchtower.name
+  pull_triggers = [data.docker_registry_image.watchtower.sha256_digest]
+}
 
-# # Create a container
-# resource "docker_container" "cloudflared" {
-#   image = docker_image.cloudflared.image_id
-#   name  = "cloudflared_tunnel"
-#   restart = "always"
-#   command = ["tunnel", "--no-autoupdate", "run", "--token", "${var.cloudflared_token}"]
-# }
-
-
-# resource "docker_image" "nginx" {
-#   name = "nginx:latest"
-# }
-
-# resource "docker_container" "testnginx" {
-#   image = docker_image.nginx.image_id
-#   name  = "test_nginx_cloudflare_app"
-#   restart = "always"
-#   ports {
-#     internal = "80"
-#     external = "8089"
-#   }
-# }
-
+resource "docker_container" "watchtower" {
+  image = docker_image.watchtower.image_id
+  name  = "watchtower"
+  restart = "always"
+  volumes {
+      host_path = "/var/run/docker.sock"
+      container_path = "/var/run/docker.sock"
+  }
+  env = [
+    "TZ=America/Denver",
+    "WATCHTOWER_CLEANUP=true",
+    "WATCHTOWER_INCLUDE_STOPPED=true",
+    "WATCHTOWER_REVIVE_STOPPED=false",
+    "WATCHTOWER_SCHEDULE=0,15,30,45 * * * *"
+  ]
+}

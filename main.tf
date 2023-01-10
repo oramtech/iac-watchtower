8cf279f217ed9db2e4a52d07a39fdd07d30bb8c5
Ben Oram <b@oram.co>
2023-01-10 14:53:51 -0500

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.24.0"
+    }
+  }
+
+  cloud {
+    organization = "oramtech"
+
+    workspaces {
+      name = "iac-watchtower"
+    }
+  }
+}
+
+provider "docker" {
+  host     = var.dockertcp_host
+
+  ca_material =  var.dockertcp_ca_material
+  cert_material = var.dockertcp_cert_material
+  key_material = var.dockertcp_key_material
+}
